5cfcd65501b08605580e47145c0b042e4cb731c7
LuxXMF <feralunsettler@gmail.com>
2026-07-24 12:55:15 -0300

    Persist Elementor Pro plugin in repository

---
+++ wp-content/plugins/elementor-pro/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/elementor-pro',
+        'pretty_version' => '4.02.x-dev',
+        'version' => '4.02.9999999.9999999-dev',
+        'reference' => '0e497198fa21851a007aaa06dd73fc409137476d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/elementor-pro' => array(
+            'pretty_version' => '4.02.x-dev',
+            'version' => '4.02.9999999.9999999-dev',
+            'reference' => '0e497198fa21851a007aaa06dd73fc409137476d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
